5f273deeae9b5f2ebb8ae5e3ccc2e5d38ae7dd37
Ernest <ernest.viol@gmail.com>
2018-11-08 16:34:34 -0800

    Created a not found page and home page

---
+++ src/Routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom'
+
+//Routes
+import Home from './containers/Home'
+import NotFound from './containers/NotFound';
+
+
+export default () =>
+    <Switch>
+        <Route path="/" exact component={Home} />
+        <Route component={NotFound} />
+    </Switch>
---
+++ src/containers/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div className="Home">
+                <h3>This is where the magic happens</h3>
+            </div>
+        )
+    }
+}
