ca96382380e9afba4d2acb01e7ad94e4504b7273
Ernest <ernest.viol@gmail.com>
2018-11-12 20:10:29 -0800

    Created a blog layout

--- src/containers/Blogs/Welcome.js
+++ src/containers/Blogs/Welcome.js
@@ -3,15 +3,29 @@ import { Grid, Row, Col, Clearfix } from 'react-bootstrap';
 
 import './Blog.css';
 
-export default class componentName extends Component {
+export default class Welcome extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            title: "Welcome",
+            subtitle: "To my Blog",
+            content: "Hello World. Let me in Bobby",
+            date: "November 12, 2018",
+            author: "Ernest Viola"
+        };
+    }
+
     render() {
         return (
             <Grid className="Blog">
                 <Row>
-                    <Col xs={12} md={8} className="Main">
-                        <h2>Welcome</h2>
+                    <Col xs={12} md={10} className="Main">
+                        <h1>{this.state.title}</h1>
+                        <h3>{this.state.subtitle}</h3>
+                        <p>{this.state.content}</p>
                     </Col>
-                    <Col xs={6} md={4} className="Sidebar">
+                    <Col xs={6} md={2} className="Sidebar">
                         <h4>Sidebar</h4>
                     </Col>
                 </Row>
