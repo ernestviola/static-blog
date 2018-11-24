a00c385c03927719f2014f9f0f0a9aa1ba3f8755
Ernest <ernest.viol@gmail.com>
2018-11-23 17:49:13 -0800

    Created the Main and Side content

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,8 +5,21 @@ import "./Home.css";
 export default class Home extends Component {
     render() {
         return (
-            <div className="Home">
-            </div>
+            <Grid>
+                <Row>
+                    <Col className="Main" xs={12} md={8}>
+                        MAIN
+                    </Col>
+                    <Col className="Side" xs={12} md={4}>
+                        <Col xs={12} sm={6} md={12}>
+                            Side 1
+                        </Col>
+                        <Col xs={12} sm={6} md={12}>
+                            Side 2
+                        </Col>
+                    </Col>
+                </Row>
+            </Grid>
         )
     }
 }
