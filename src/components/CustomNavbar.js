2d445d4809010fc174c95fe7efff760400d5f714
Ernest <ernest.viol@gmail.com>
2018-11-23 16:03:50 -0800

    Added css to custom navbar

--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Nav, Navbar, NavItem } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './CustomNavbar.css';
 
 export default class CustomNavbar extends Component {
     render() {
