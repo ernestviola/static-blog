1a9dc07e4540ffe5a69678329b34828f1ae8ea75
Ernest <ernest.viol@gmail.com>
2018-12-10 19:33:34 -0800

    Added content to the main page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -13,9 +13,25 @@ export default class Home extends Component {
                     <Col className="Side-Content" xs={12} md={4}>
                         <Col xs={12} sm={6} md={12}>
                             <h3>Projects</h3>
+                            <ul>
+                                <li>Flys</li>
+                                <li>Radio Listener</li>
+                                <li>Personal Blog</li>
+                                <li>Player's Known BattleShips Game</li>
+                                <li>Fireman Catch</li>
+                                <li>Bash shell project</li>
+                            </ul>
                         </Col>
                         <Col xs={12} sm={6} md={12}>
                             <h3>Tasks</h3>
+                            <ul>
+                                <li>Add an image and description to Home's Main Content</li>
+                                <li>Write First Blog</li>
+                                <li>Create a portfolio page with resume</li>
+                                <li>Add a contact me</li>
+                                <li>Add demos to all projects with links to github</li>
+                                <li>Finish the Readme.md for all projects on github</li>
+                            </ul>
                         </Col>
                     </Col>
                 </Row>
