a16dacc4c876a92ccebb6610b4035edab48f6e5b
Ernest <ernest.viol@gmail.com>
2018-12-21 15:49:02 -0800

    Added a background color to the app

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-
+import './Home.css';
 export default class componentName extends Component {
     render() {
         return (
-            <div className="Home">
+            <div className="Home container">
                 I'm home...
             </div>
         )
