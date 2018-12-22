813d43447cfada2e66283659a6b8ee041c086e47
Ernest <ernest.viol@gmail.com>
2018-12-21 22:50:16 -0800

    Fixed security error

--- src/components/customFooter.js
+++ src/components/customFooter.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import './customFooter.css';
 export default class CustomFooter extends Component {
     render() {
@@ -8,7 +7,7 @@ export default class CustomFooter extends Component {
                 <hr />
                 <div className="container">
                     <div className="ForterExternalLinks">
-                        <a target="_blank" href="https://github.com/ernestviola">Github</a> <a target="_blank" href="https://linkedin.com/in/ernestviola">LinkedIn</a>
+                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/ernestviola">Github</a> <a target="_blank" rel="noopener noreferrer" href="https://linkedin.com/in/ernestviola">LinkedIn</a>
                     </div>
                     <div>
                         2018-{(new Date().getFullYear())} © Ernest Viola
