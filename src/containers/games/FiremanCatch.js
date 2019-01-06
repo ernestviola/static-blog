f585eb0e43836568da6b76b5c525029db167475e
Ernest <ernest.viol@gmail.com>
2019-01-05 16:52:22 -0800

    added an iframe title

--- src/containers/games/FiremanCatch.js
+++ src/containers/games/FiremanCatch.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class FiremanCatch extends Component {
     render() {
         return (
-            <iframe frameborder="0" src="https://itch.io/embed-upload/1207452?color=333333" allowfullscreen="" width="980" height="680"></iframe>
+            <iframe title="Fireman Catch" frameborder="0" src="https://itch.io/embed-upload/1207452?color=333333" allowfullscreen="" width="980" height="680"></iframe>
         )
     }
 }
