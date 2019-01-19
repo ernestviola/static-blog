fade19ae1cb4a0877e19346d1348ee90e8cafd79
Ernest <ernest.viol@gmail.com>
2019-01-18 16:26:56 -0800

    Changed footer style

--- src/components/customFooter.js
+++ src/components/customFooter.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './customFooter.css';
+import resume from '../images/Resume.pdf';
 export default class CustomFooter extends Component {
     render() {
         return (
             <div className="Footer">
-                <div className="FooterExternalLinks">
+                <div className="FooterLinks">
+                    <ul className="ExternalLinks">
+                        <li>
+                            <a target="_blank" rel="noopener noreferrer" href="https://github.com/ernestviola">Github</a>
+                        </li>
 
-                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/ernestviola">Github</a> <a target="_blank" rel="noopener noreferrer" href="https://linkedin.com/in/ernestviola">LinkedIn</a>
-                    {(new Date().getFullYear())} © <a className="footerTitle" to="/">Ernest Viola</a>
+                        <li>
+                            <a target="_blank" rel="noopener noreferrer" href="https://linkedin.com/in/ernestviola"> LinkedIn</a>
+                        </li>
+                    </ul>
+                    <ul className="InternalLinks">
+                        <li>
+                            <a to="/projects">Projects </a>
+                        </li>
+                        <li>
+                            <a target="blank_" href={resume}>Resume </a>
+                        </li>
+                        <li>
+                            {(new Date().getFullYear())} © <a to="/">Ernest Viola</a>
+                        </li>
+                    </ul>
+
+
 
 
 
