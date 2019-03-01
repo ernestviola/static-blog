d06eb498f1152dbbd51de5b25838bbabd8b541eb
Ernest <ernest.viol@gmail.com>
2019-02-28 21:00:56 -0800

    Setting up ProjectCards to hold the cards containing each project

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import ProjectCards from '../components/ProjectCards';
 
 export default class Projects extends Component {
     render() {
         return (
             <div className="Projects container">
+                <ProjectCards />
                 <div>
                     <h3>Projects</h3>
                     <ul>
