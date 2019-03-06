a940d7f9e65367c0928821a18603f4b21e46d925
Ernest Viola <ernest.viol@gmail.com>
2019-03-06 11:46:06 -0800

    Removed unnecessary photos

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,27 +1,13 @@
 import React, { Component } from 'react'
 import { Button, Col, Row, Container } from 'react-bootstrap';
+import Graduation from '../images/Graduations-24.jpg';
 import './Home.css';
-import Graduation from '../images/homeCarousel/Graduations-24.jpg';
 
 
 export default class Home extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.images = {
-    //         1: 'src/images/homeCarousel/50307388_00801_0589_Large.jpg',
-    //         2: 'src/images/homeCarousel/DSC03556.JPG',
-    //         3: 'src/images/homeCarousel/DSC03573.JPG',
-    //         4: 'src/images/homeCarousel/DSC03582.JPG',
-    //         5: 'src/images/homeCarousel/DSC03665.JPG',
-    //         6: 'src/images/homeCarousel/DSC03709.JPG',
-    //         7: 'src/images/homeCarousel/DSC03720.JPG',
-    //         8: 'src/images/homeCarousel/DSC03734.JPG',
-    //         9: 'src/images/homeCarousel/DSC03736.JPG',
-    //         10: 'src/images/homeCarousel/DSC03742.JPG',
-    //         11: 'src/images/homeCarousel/DSC04315.JPG',
-    //         12: 'src/images/homeCarousel/Graduations-24.jpg'
-    //     }
-    // }
+    constructor(props) {
+        super(props);
+    }
 
     render() {
         return (
