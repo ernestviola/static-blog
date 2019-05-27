0884aa993c7f9c5513ec0f72451fb22c1c4f2f3c
Ernest Viola <ernest.viol@gmail.com>
2019-05-27 15:28:32 -0700

    Got it working

---
+++ src/Routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './containers/Home';
+// import Projects from './containers/Projects';
+// import FiremanCatch from './containers/games/FiremanCatch';
+// import NotFound from './containers/NotFound';
+
+export default () =>
+    <Switch>
+        <Route path="/" exact component={Home} />
+        {/* <Route path="/projects" exact component={Projects} />
+        <Route path="/games/firemancatch" exact component={FiremanCatch} />
+        <Route component={NotFound} /> */}
+    </Switch>
---
+++ src/containers/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+// import './Home.css';
+
+
+export default class Home extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="Home">
+
+            </div>
+
+        )
+    }
+}
