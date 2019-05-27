8b250cd0131e1ff8c9e214d1e4c09c4e8522a733
Ernest Viola <ernest.viol@gmail.com>
2019-05-27 13:30:32 -0700

    Restarting

--- src/containers/games/FiremanCatch.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class FiremanCatch extends Component {
-    render() {
-        return (
-            <iframe title="Fireman Catch" frameborder="0" src="https://itch.io/embed-upload/1207452?color=333333" allowfullscreen="" width="980" height="680"></iframe>
-        )
-    }
-}
