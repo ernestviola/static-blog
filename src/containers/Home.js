310bd715e0bf758eb69bfeacc7b592979f480c1f
Ernest Viola <ernest.viol@gmail.com>
2019-05-28 18:44:21 -0700

    Worked on homepage

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,12 +5,27 @@ import React, { Component } from 'react'
 export default class Home extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            currentQuote: "",
+            quotes: [
+                "I'm working on my posture."
+            ]
+        }
+    }
+
+    componentDidMount() {
+        this.setState({ currentQuote: this.state.quotes[this.randomQuote()] })
+    }
+
+    randomQuote() {
+        return Math.floor(Math.random() * Math.floor(this.state.quotes.length));
     }
 
     render() {
         return (
-            <div className="Home">
-
+            <div className="Home container">
+                <h1>Hi, I'm Ernest & <span>{this.state.currentQuote}</span></h1>
             </div>
 
         )
